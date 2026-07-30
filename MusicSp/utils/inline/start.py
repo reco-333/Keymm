8eaec931520595812a5f5c26fface729128bd1fd
reco-333 <core95403@gmail.com>
2026-07-30 09:38:33 -0600

    DEV>KHH

--- MusicSp/utils/inline/start.py
+++ MusicSp/utils/inline/start.py
@@ -29,6 +29,6 @@ def private_panel(_):
             InlineKeyboardButton(text=_["S_B_6"], url=config.SUPPORT_CHANNEL),
             InlineKeyboardButton(text=_["S_B_2"], url=config.SUPPORT_GROUP),
         ],
-        [InlineKeyboardButton(text="𝕮_𝕺_𝕽_𝕰", url="https://t.me/+73UOpH8smTplYTE9")],
+        [InlineKeyboardButton(text="Owner", url="https://t.me/GodfatherRECO")],
     ]
     return buttons
